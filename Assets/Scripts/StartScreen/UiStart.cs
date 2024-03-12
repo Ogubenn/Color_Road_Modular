be3603bd29b77f5205df049e6c455cfc9ec950f2
Oğulcan Durkan <ogulcan285@outlook.com>
2024-03-13 00:27:53 +0300

    Music and sfx slider startmenu settings

--- Assets/Scripts/StartScreen/UiStart.cs
+++ Assets/Scripts/StartScreen/UiStart.cs
@@ -3,15 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class UiStart : MonoBehaviour
 {
     public GameObject startPanel;
     public GameObject settingsPanel;
+
+    [Header("Audio")]
+    [SerializeField] private Slider MusicSlider;
+    [SerializeField] private Slider SFXSlider;
+    [SerializeField] private AudioMixer myMixer;
+
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("musicVolume"))
+            LoadVolume();
+
+        else
+        {
+            SetMusicVolume();
+            SetSFXVolume();
+        }
+    }
+
+    #region Slider fonk.
+
+    public void SetMusicVolume()
+    {
+        float volume = MusicSlider.value;
+        myMixer.SetFloat("music", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+    public void SetSFXVolume()
+    {
+        float volume = SFXSlider.value;
+        myMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+    }
+    public void LoadVolume()
+    {
+        MusicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+        SetMusicVolume();
+        SetSFXVolume();
+    }
+    #endregion
+
+
+    #region Button fonk.
     public void StartButton()
     {
         SceneManager.LoadScene(1);
     }
+   
 
     public  void SettingsButtonOn()
     {
@@ -23,6 +69,8 @@ public class UiStart : MonoBehaviour
         settingsPanel.SetActive(false);
         startPanel.SetActive(true);
     }
+
+    #endregion
 
 
 
